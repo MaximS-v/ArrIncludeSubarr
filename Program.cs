9d99951ee939af419f731410c109d6950569daa3
Max Safronov <bmc938@gmail.com>
2022-03-03 19:47:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrIncludeSubarr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите меньший массив:");
+            var strSmallArr = Console.ReadLine().Split(' ');
+            Console.WriteLine("Введите больший массив:");
+            var strBigArr = Console.ReadLine().Split(' ');
+            int currSmallArrNum = 0;
+            for (int i = 0; i < strBigArr.Length; i++)
+            {
+                if (currSmallArrNum < strSmallArr.Length)
+                {
+                    if (strBigArr[i] == strSmallArr[currSmallArrNum])
+                        currSmallArrNum++;
+                }
+                else
+                    break;
+            }
+            if (currSmallArrNum == strSmallArr.Length)
+                Console.WriteLine("Меньший массив входит в больший как подпоследовательность");
+            else
+                Console.WriteLine("Меньший массив не входит в больший как подпоследовательность");
+        }
+    }
+}
